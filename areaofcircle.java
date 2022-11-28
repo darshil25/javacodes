9e3c452858b46778d5952aa840cf2f7435e3869f
Darshil Thummar <thummardarshil9687@gmail.com>
2022-11-28 11:02:13 +0530

    Area of Circle in Java

---
+++ areaofcircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class areaofcircle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float r = sc.nextFloat();
+        float A = 3.14f*r*r;
+        System.out.println(A);
+    }
+}
