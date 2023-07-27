7f3a7b265585d87d0dca19232e9612c0d8babe7c
Darshil Thummar <thummardarshil9687@gmail.com>
2023-07-27 22:05:27 +0530

    Add files via upload

--- Q5.java
+++ Q5.java
@@ -1,12 +1,24 @@
-public class Q5{
+public class Q5 {
     public static void main(String[] args) {
-        double x1 = 7;
-        double x2 = 8;
-        double y1 = 5;
-        double y2 = 6;
-        double w1 = Math.pow(x2-x1, 2);
-        double w2 = Math.pow(y2-y1, 2);
-        double distance = Math.sqrt(w1+w2);
-        System.out.println("distance is "+distance);
+        Vehicle v1 = new Car();
+        v1.print();
     }
 }
+
+class Vehicle{
+    void print(){
+        System.out.println("Base Class Vehicle");
+    }
+}
+
+class Car extends Vehicle{
+    void print(){
+        System.out.println("Derived class Car");
+    }
+}
+//Output will be taken from print function of Car class
+/*
+ * Reason - in Car class there are two function of print 1. from Vehicle class and 2. it's own
+ *          Because of function overriding but in car class object's its own print() function will be called
+ *          That's why it prints "Derived class Car"
+ */
