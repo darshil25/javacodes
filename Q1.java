7d104a36d421a71c1baf01068a4248e54d4f869e
Darshil Thummar <thummardarshil9687@gmail.com>
2023-07-27 21:26:08 +0530

    Add files via upload

--- Q1.java
+++ Q1.java
@@ -1,37 +1,19 @@
-import java.awt.*;
-import java.awt.event.*;
+/*
+ * Assign name to object
+ */
 
-public class Q1 extends Frame implements MouseListener{
-    Label lb;
-    
-    Q1() {
-        addMouseListener(this);
 
-        lb = new Label();
-        lb.setBounds(20, 50, 100, 100);
-        add(lb);
-        setSize(700, 500);
-        setLayout(null);
-        setVisible(true);
-    }
+class Student{
+    String name;
+    int marks;
+}
 
-    public void mouseClicked(MouseEvent e){
-        lb.setText("Mouse clicked");
-    }
-    public void mouseEntered(MouseEvent e){
-        lb.setText("Mouse Entered");
-    }
-    public void mouseExited(MouseEvent e){
-        lb.setText("Mouse Exited");
-    }
-    public void mousePressed(MouseEvent e){
-        lb.setText("Mouse Pressed");
-    }
-    public void mouseReleased(MouseEvent e){
-        lb.setText("Mouse Released");
-    }
-
+public class Q1 {
     public static void main(String[] args) {
-        new Q1();
+        Student s1 = new Student();
+
+        //Code 
+        s1.name = "Darshil";
+        System.out.println(s1.name);
     }
 }
