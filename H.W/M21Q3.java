b6228badb3a02789683e38e12394d4f8dd2c8b2f
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-04 19:10:39 +0530

    Update M21Q3.java

--- H.W/M21Q3.java
+++ H.W/M21Q3.java
@@ -1,5 +1,5 @@
 public class M21Q3 {
-
+    //Brute Force (O(n^2))
     public static int quickSort(int arr[]){
         int count = 0;
         for(int i=0; i<arr.length-1; i++){
@@ -11,6 +11,7 @@ public class M21Q3 {
         }
         return count;
     }
+
     public static void main(String[] args) {
         int arr[] = {2, 4, 1, 3, 5};
         System.out.println(quickSort(arr));
