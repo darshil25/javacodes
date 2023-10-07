f666c9a9ead0f3cf0fa5fe2747760389a05abebe
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-07 11:09:17 +0530

    Update Find_SubSets.java

--- Back Tracking/Find_SubSets.java
+++ Back Tracking/Find_SubSets.java
@@ -1,24 +1,28 @@
 public class Find_SubSets {
 
-    public static void subSet(String s, String ans, int i){
+    public static void subSet(String s, String ans, int i) {
 
-        //base case
-        if(i == s.length()){
-            System.out.println(ans);
+        // base case
+        if (i == s.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null set");
+            } else {
+                System.out.println(ans);
+            }
             return;
         }
 
-        //recursion
-        //Choice = yes
-        subSet(s, ans+s.charAt(i), i+1);
+        // recursion
+        // Choice = yes
+        subSet(s, ans + s.charAt(i), i + 1);
 
-        //choice = no
-        subSet(s, ans, i+1);
+        // choice = no
+        subSet(s, ans, i + 1);
     }
+
     public static void main(String[] args) {
         String s = "abc";
         String ans = "";
-        int i=0;
-        subSet(s, ans, i);
+        subSet(s, ans, 0);
     }
 }
