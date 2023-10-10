dded106c9fe8fd140fc336680122cf0e0d967537
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-10 12:32:14 +0530

    Create Grid_Ways_Optimized.java

---
+++ Back Tracking/Grid_Ways_Optimized.java
@@ -0,0 +1,20 @@
+public class Grid_Ways_Optimized {
+
+    public static int permutation(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*permutation(n-1);
+    }
+    public static int gridways(int n, int m){
+
+        int w1 = permutation(n-1); //for down
+        int w2 = permutation(m-1); //for right
+
+        return ((permutation(w1+w2))  / (permutation(w1)*permutation(w2)));
+    }
+    public static void main(String[] args) {
+        int n = 3000, m = 3333;
+        System.out.println(gridways( n, m));
+    }
+}
