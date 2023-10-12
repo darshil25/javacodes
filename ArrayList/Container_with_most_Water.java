ba6929f06981e99f729cfd31ab155e2768fb9e8e
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-12 16:01:52 +0530

    Update Container_with_most_Water.java

--- ArrayList/Container_with_most_Water.java
+++ ArrayList/Container_with_most_Water.java
@@ -4,13 +4,33 @@ public class Container_with_most_Water {
 
     public static int trappedwater(ArrayList<Integer> height){
 
-        //bruteforce
         int max = 0;
-        for(int i=0; i<height.size(); i++){
-            for(int j=i+1; j<height.size(); j++){
-                int h = Math.min(height.get(i), height.get(j));
-                int w = j-i;
-                max = Math.max(max, h*w);
+        //bruteforce O(N^2)
+        // for(int i=0; i<height.size(); i++){
+        //     for(int j=i+1; j<height.size(); j++){
+        //         int h = Math.min(height.get(i), height.get(j));
+        //         int w = j-i;
+        //         max = Math.max(max, h*w);
+        //     }
+        // }
+
+        //2 pointers approach O(N)
+        int left = 0;
+        int right = height.size()-1;
+
+        while(left < right){
+            //get water are
+            int h = Math.min(height.get(left), height.get(right));
+            int w = right - left;
+            int current = h*w;
+            max = Math.max(max, current);
+
+            //update pointers
+            if(left < right){
+                left++;
+            }
+            else{
+                right--;
             }
         }
         return max;
