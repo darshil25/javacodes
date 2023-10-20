342d9582a5234863e1591bfae8bdaf212ff8118d
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-10-20 10:55:56 +0530

    Create intro_LL.java

---
+++ LinkedLists/intro_LL.java
@@ -0,0 +1,18 @@
+/**
+ * intro_LL
+ */
+public class intro_LL {
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){ //this is constructor
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
